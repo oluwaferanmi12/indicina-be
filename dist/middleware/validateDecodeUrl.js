add721bc24dd806da8e05b70a03b3a09d013dd99
Mark <feranmijogbodo@gmail.com>
2025-05-05 08:34:28 +0100

    latest build for typescript

--- dist/middleware/validateDecodeUrl.js
+++ dist/middleware/validateDecodeUrl.js
@@ -4,12 +4,12 @@ exports.validateDecodeUrl = void 0;
 const validate_url_1 = require("../utils/validate-url");
 const validateDecodeUrl = (req, res, next) => {
     const { url } = req.body;
+    console.log(url, "Url Value here");
     const validate = (0, validate_url_1.validateUrl)(url);
     if (validate.validated) {
         // split the url to get the short code and if for any reason there's more than one path param it automatically should fail 
         // Now check if the short matches anything in the data
         const shortCode = (0, validate_url_1.extractShortCodeFromShortUrl)(url);
-        console.log(shortCode, "Short code value ---- here");
         if (!shortCode) {
             res.status(400).send({ message: "invalid url" });
         }
